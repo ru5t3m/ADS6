c5f297913b1b1bc0090f5bb5fdbf0723ee78c0a8
ru5t3m <zharmagambetovrustem@gmail.com>
2023-05-31 22:16:09 +0600

    Edge

--- src/com/company/Edge.java
+++ src/com/company/Edge.java
@@ -1,4 +1,13 @@
 package com.company;
 
-public class Edge {
+public class Edge<V> {
+    private Vertex<V> source;
+    private Vertex<V> dest;
+    private Double weight;
+
+    public Edge(Vertex source, Vertex dest, Double weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
 }
